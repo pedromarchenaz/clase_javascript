29b8ac1f03b8e4e802a2de5aa13cb490e9d188da
Pedro Marchana <pmarchena1@cuc.edu.co>
2021-05-03 19:41:36 -0500

    Comentarios, variables, cadenas, constantes

---
+++ script.js
@@ -0,0 +1,37 @@
+//Hola Mundo
+alert('Hola Mundo');
+console.log('Hola Mundo');
+
+// comentario de linea
+/*
+comentario de
+varias lineas
+*/
+
+console.log('Linea uno');
+console.log('linea  dos');
+
+// variables
+let a = 5;
+console.log(a);
+
+// cadenas
+let nombre = 'Pedro';
+console.log("Hola mundo", nombre);
+console.log('Hola mundo'+nombre);
+
+let b = 4;
+let c  = 5;
+let d = 6;
+
+let nombreCompleto = 'Pedro Marchena';
+let nombre_completo = 'Pedro Marchena';
+
+let $ = 1;
+let _ = 2;
+
+// constantes
+const PI = 3.1416;
+const COLOR_BLANCO =  '#FFFFFF';
+//PI = 5;
+//const codigo = getId();
